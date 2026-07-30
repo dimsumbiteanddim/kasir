a939fc085375e1f4f2a17188aa20fca830428cfe
dimsumbiteanddim <siddiqmadrid@gmail.com>
2026-07-30 11:07:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,2 @@
 // Service Worker Minimalis untuk Bite & Dim PWA
-const CACHE_NAME = 'bitedim-cashier-v1';
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan request langsung mengambil dari jaringan online/cloud Sheets
-  return;
-});
+c
